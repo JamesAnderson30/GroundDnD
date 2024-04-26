064565e4ce178e84329176fef972ced416749b1b
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-04-25 23:56:30 -0400

    Quick test on spot route

---
+++ backend/routes/api/spot.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { Op } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+const spot = require('../../db/models/spot');
+
+const router = express.Router();
+
+router.get("/current", async (req,res)=>{
+    let test = await spot.findAll();
+    res.json(spot);
+})
+
+module.exports = router;
