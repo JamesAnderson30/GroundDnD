d0d5e01df21747f96e267815c85e52d356c0427e
JamesAnderson30 <145408914+JamesAnderson30@users.noreply.github.com>
2024-04-25 22:38:37 -0400

    Update spot.js
    Commenting out associations for now

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -9,24 +9,24 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-      spot.belongsToMany(models.user, {
-        foreignKey: "ownerId"
-      }),
+    // static associate(models) {
+    //   // define association here
+    //   spot.belongsToMany(models.user, {
+    //     foreignKey: "ownerId"
+    //   }),
 
-      spot.hasMany(models.review, {
-        foreignKey: "spotId"
-      }),
+    //   spot.hasMany(models.review, {
+    //     foreignKey: "spotId"
+    //   }),
 
-      spot.hasMany(models.spotsImages, {
-        foreignKey: "spotId"
-      }),
+    //   spot.hasMany(models.spotsImages, {
+    //     foreignKey: "spotId"
+    //   }),
 
-      spot.hasMany(models.booking, {
-        foreignKey: "spotId"
-      })
-    }
+    //   spot.hasMany(models.booking, {
+    //     foreignKey: "spotId"
+    //   })
+   // }
   }
   spot.init({
     ownerId: DataTypes.INTEGER,
