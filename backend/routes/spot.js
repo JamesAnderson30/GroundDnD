c448a8bbe9efe5f3b919a0e4c3ab9af49ec33822
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-04-27 17:09:49 -0400

    spot route debug

--- backend/routes/spot.js
+++ backend/routes/spot.js
@@ -18,7 +18,7 @@ router.get("/current", async (req,res)=>{
 
     let userId = req.user.id;
     //console.log(Models);
-    console.log(Review);
+    console.log(Models);
     let spots = await Spot.findAll({
         include:{
             model: Review,
