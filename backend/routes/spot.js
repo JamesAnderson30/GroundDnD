44a27f50e44a6d754e1ebf27c3071d5c3b685221
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-04-27 14:37:49 -0400

    check

--- backend/routes/spot.js
+++ backend/routes/spot.js
@@ -6,15 +6,22 @@ const { handleValidationErrors } = require('../utils/validation');
 //const { restoreUser } = require("../../utils/auth.js");
 const { setTokenCookie, restoreUser } = require('../utils/auth');
 const { Spot } = require('../db/models');
-const Models = require("../db/models")
+const Models = require("../db/models");
+const review = require('../db/models/review');
 
 const router = express.Router();
 //console.log(Models);
 router.get("/current", async (req,res)=>{
     //console.log(Models);
-    let test = await Spot.findAll();
+    let spots = await Spot.findAll({
+        include:{
+            model: review,
+            attributes: stars
+        }
+    });
+
     //console.log(test);
-    res.json(test);
+    res.json(spots);
 })
 
 module.exports = router;
