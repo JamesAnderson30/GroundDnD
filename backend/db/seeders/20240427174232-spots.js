339d568717c9b3e364c31452d12d7b837916261d
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-04-29 23:30:07 -0400

    debugging spots seeder

--- backend/db/seeders/20240427174232-spots.js
+++ backend/db/seeders/20240427174232-spots.js
@@ -26,7 +26,7 @@ module.exports = {
         name: "Some Spot",
         description: "Description",
         price: 20.20
-      }], {});
+      }], {logging: console.log});
 
   },
   // ownerId: DataTypes.INTEGER,
