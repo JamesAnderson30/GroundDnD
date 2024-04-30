4f73ddb7fbd86a71ec4bf2f18c1de00ae5d03bf1
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-04-29 21:06:50 -0400

    end-debug-don't-do-again

--- backend/db/models/Spot.js
+++ backend/db/models/Spot.js
@@ -13,9 +13,12 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Spot.hasMany(models.Booking, {
-        foreignKey: "ownerId"
+        foreignKey: "spotId"
       })
 
+      Spot.hasMany(models.Review,{
+        foreignKey: "spotId"
+      })
     // //   Spot.hasMany(models.review, {
     // //     foreignKey: "SpotId"
     // //   }),
