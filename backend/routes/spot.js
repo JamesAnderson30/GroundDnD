cd8be9dcbe4180a094b5fa94da7d85870ff56f08
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-04-30 20:58:19 -0400

    test

--- backend/routes/spot.js
+++ backend/routes/spot.js
@@ -4,20 +4,27 @@ const bcrypt = require('bcryptjs');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../utils/validation');
 //const { restoreUser } = require("../../utils/auth.js");
-const { setTokenCookie, restoreUser } = require('../utils/auth');
+const { setTokenCookie, restoreUser, requireAuth } = require('../utils/auth');
 const { Spot, Review } = require('../db/models');
 const Models = require("../db/models");
 const review = require('../db/models/review');
 const router = express.Router();
 //console.log(Models);
+
+
 router.get("/current", async (req,res)=>{
 
-
+requireAuth(req,res,(err)=>{
+    res.json(["Hecc"]);
+});
     let userId = req.user.id;
+    console.log(userId);
     //console.log(Models);
     // res.contentType("text/plain")
     // console.log(Models.Review);
     // res.send(Models.models)
+
+
 
     let spots = await Spot.findAll({
         include:{
@@ -46,9 +53,9 @@ router.get("/current", async (req,res)=>{
 
         spotsArray.push(spot);
     }
-    }
 
-    res.json(spotsArray);
+
+    res.json(["unga", "bunga"]);
 
     //console.log(test);
     //res.json(spots);
