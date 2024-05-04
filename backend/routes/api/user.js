cdf643d38e1583d36346a4936204fc2df07f9d02
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-05-04 17:07:20 -0400

    Debugging user Id duplicating problem

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -71,7 +71,7 @@ router.post(
       }
 
       //If no errors are found
-      const user = await User.create({ email:email, username:username, hashedPassword:hashedPassword, firstName:firstName, lastName:lastName });
+      const user = await User.create({ email, username, hashedPassword, firstName, lastName }, {fields:[]});
 
       const safeUser = {
         id: user.id,
