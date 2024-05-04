9cab5c459abab1ffab5438fe8f1a02e5c8d562b1
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-05-04 13:15:24 -0400

    mysterious extra column debugging

--- backend/db/models/User.js
+++ backend/db/models/User.js
@@ -5,7 +5,9 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // define association here
-      User.hasMany(models.Spot);
+      User.hasMany(models.Spot,{
+        foreignKey:"ownerId"
+      });
 
     }
   };
