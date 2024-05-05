a3968a26f54fd436c41bb9d7560dad9acbdadae9
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-05-05 16:05:36 -0400

    debugged booking migration

--- backend/db/migrations/20240505015029-create-booking.js
+++ backend/db/migrations/20240505015029-create-booking.js
@@ -38,13 +38,13 @@ module.exports = {
         type: Sequelize.DATE
       },
       createdAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       },
       updatedAt: {
 
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       }
