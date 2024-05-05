04e15f6bf8591a3d5a83bdcbc80950a640bafd1c
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-05-05 16:11:03 -0400

    removed duplicate migration

---
+++ backend/db/migrations/20240426023206-create-booking.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let options = {
+  logging:true
+};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  // async up(queryInterface, Sequelize) {
+  //   await queryInterface.createTable('Bookings', {
+  //     id: {
+  //       allowNull: false,
+  //       autoIncrement: true,
+  //       primaryKey: true,
+  //       type: Sequelize.INTEGER,
+  //       onDelete: "CASCADE"
+  //     },
+  //     userId: {
+  //       type: Sequelize.INTEGER
+  //     },
+  //     spotId: {
+  //       type: Sequelize.INTEGER
+  //     },
+  //     startDate: {
+  //       type: Sequelize.DATE
+  //     },
+  //     endDate: {
+  //       type: Sequelize.DATE
+  //     },
+  //     createdAt: {
+  //       allowNull: false,
+  //       type: Sequelize.DATE
+  //     },
+  //     updatedAt: {
+  //       allowNull: false,
+  //       type: Sequelize.DATE
+  //     }
+  //   }, options);
+  // },
+  // async down(queryInterface, Sequelize) {
+  //   options.tableName = "Bookings";
+  //   await queryInterface.dropTable('bookings');
+  //   await queryInterface.dropTable(options)
+  // }
+};
