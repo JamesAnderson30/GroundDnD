c30c5b863ff8d28525d3c030367c492a6b39ac20
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-05-05 01:39:03 -0400

    quick migration fix

--- backend/db/seeders/20240505015332-create-booking.js
+++ backend/db/seeders/20240505015332-create-booking.js
@@ -37,7 +37,7 @@ module.exports = {
       spotId:1,
       startDate: new Date("2024-03-25"),
       endDate: new Date("2024-03-25")
-    }])
+    }], options)
   },
 
   async down (queryInterface, Sequelize) {
