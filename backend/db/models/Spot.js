13eebdeca0190556d5a42dfa766df73a864fd7b3
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-05-05 01:21:54 -0400

    quick migration fix

--- backend/db/models/Spot.js
+++ backend/db/models/Spot.js
@@ -31,7 +31,7 @@ module.exports = (sequelize, DataTypes) => {
       })
 
       Spot.hasMany(models.Booking, {
-        foreignKey: 'spotId',
+        sourceKey: 'spotId',
       })
     // //   Spot.hasMany(models.review, {
     // //     foreignKey: "SpotId"
