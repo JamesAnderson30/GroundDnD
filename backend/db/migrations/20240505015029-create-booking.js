5ba8ffdcda095fdc2f9320fe05f1df0dad51082c
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-05-05 16:15:59 -0400

    removed duplicate migration

--- backend/db/migrations/20240505015029-create-booking.js
+++ backend/db/migrations/20240505015029-create-booking.js
@@ -40,14 +40,14 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
-
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
+
     },options);
   },
   async down(queryInterface, Sequelize) {
