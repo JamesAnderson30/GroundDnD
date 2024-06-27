24c7cb14efa456999b309ddbc728b021ab4dea12
JamesAnderson30 <jamesanderson_18@hotmail.com>
2024-06-26 23:09:22 -0400

    Added signup

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,10 +2,9 @@ import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import {thunk} from 'redux-thunk';
 import sessionReducer from './session';
 
-
 const rootReducer = combineReducers({
-  sessionState: sessionReducer
-})
+  session: sessionReducer
+});
 
 let enhancer;
 if (import.meta.env.MODE === 'production') {
